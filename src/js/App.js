29cf21e8112495a62f9c3f067471caf56df92414
vanba278 <nguyenvanbapc66@gmail.com>
2020-10-16 23:54:47 +0700

    start project

---
+++ src/js/App.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
